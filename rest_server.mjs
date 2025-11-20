56b07c41dadf4c3522a06f24d2ee06e2fbeb37a3
Anastasia Iogova <iogo3143@stthomas.edu>
2025-11-19 23:13:01 -0600

    Query params for GET /incidents almost fully working

--- rest_server.mjs
+++ rest_server.mjs
@@ -142,10 +142,166 @@ app.get('/neighborhoods', (req, res) => {
 app.get('/incidents', (req, res) => {
     console.log(req.query); // query object (key-value pairs after the ? in the url)
     
-    //let sql = 'SELECT * FROM Incidents LIMIT 1000';
-    let sql = 'SELECT case_number, date(date_time) AS date, time(date_time) AS time, code, incident, police_grid, neighborhood_number, block FROM Incidents ORDER BY date_time DESC LIMIT 1000';
+    let start_dates;
+    let end_dates;
+    let codes;
+    let grids;
+    let neighborhoods;
+    let limit = 1000;
+    let placeholders = '';
+    let queryParamCount = 0; // 'limit' doesn't count
+    let params = [];
 
-    dbSelect(sql, [])
+    let sql = 'SELECT case_number, date(date_time) AS date, time(date_time) AS time, code, incident, police_grid, neighborhood_number, block FROM Incidents';
+
+    // if 'start_date' exists as a query parameter
+    if ('start_date' in req.query) { 
+        queryParamCount++;
+        start_dates = req.query.start_date.split(',');
+        for (let i = 0; i < start_dates.length; i++) {
+            start_dates[i] = parseInt(start_dates[i]);
+            // if it's the last item of the list
+            if (i === start_dates.length - 1) {
+                placeholders += '?';
+            }
+            // if it's NOT the last item of the list
+            else {
+                placeholders += '?, ';
+            }
+        }
+
+        if (queryParamCount === 1) {
+            sql += ' WHERE start_date IN (' + placeholders + ')';
+        }
+        else {
+            sql += ' AND start_date IN (' + placeholders + ')';
+        }
+
+        placeholders = '';
+        params = params.concat(start_dates);
+    }
+
+
+    // if 'end_date' exists as a query parameter
+    if ('end_date' in req.query) { 
+        queryParamCount++;
+        end_dates = req.query.end_date.split(',');
+        for (let i = 0; i < end_dates.length; i++) {
+            end_dates[i] = parseInt(end_dates[i]);
+            // if it's the last item of the list
+            if (i === end_dates.length - 1) {
+                placeholders += '?';
+            }
+            // if it's NOT the last item of the list
+            else {
+                placeholders += '?, ';
+            }
+        }
+
+        if (queryParamCount === 1) {
+            sql += ' WHERE end_date IN (' + placeholders + ')';
+        }
+        else {
+            sql += ' AND end_date IN (' + placeholders + ')';
+        }
+
+        placeholders = '';
+        params = params.concat(end_dates);
+    }
+
+
+    // if 'code' exists as a query parameter
+    if ('code' in req.query) { 
+        queryParamCount++;
+        codes = req.query.code.split(',');
+        for (let i = 0; i < codes.length; i++) {
+            codes[i] = parseInt(codes[i]);
+            // if it's the last item of the list
+            if (i === codes.length - 1) {
+                placeholders += '?';
+            }
+            // if it's NOT the last item of the list
+            else {
+                placeholders += '?, ';
+            }
+        }
+
+        if (queryParamCount === 1) {
+            sql += ' WHERE code IN (' + placeholders + ')';
+        }
+        else {
+            sql += ' AND code IN (' + placeholders + ')';
+        }
+
+        placeholders = '';
+        params = params.concat(codes);
+    }
+
+    // if 'grid' exists as a query parameter
+    if ('grid' in req.query) { 
+        queryParamCount++;
+        grids = req.query.grid.split(',');
+        for (let i = 0; i < grids.length; i++) {
+            grids[i] = parseInt(grids[i]);
+            // if it's the last item of the list
+            if (i === grids.length - 1) {
+                placeholders += '?';
+            }
+            // if it's NOT the last item of the list
+            else {
+                placeholders += '?, ';
+            }
+        }
+
+        if (queryParamCount === 1) {
+            sql += ' WHERE police_grid IN (' + placeholders + ')';
+        }
+        else {
+            sql += ' AND police_grid IN (' + placeholders + ')';
+        }
+
+        placeholders = '';
+        params = params.concat(grids);
+    }
+
+    // if 'neighborhood' exists as a query parameter 
+    if ('neighborhood' in req.query) { 
+        queryParamCount++;
+        neighborhoods = req.query.neighborhood.split(',');
+        for (let i = 0; i < neighborhoods.length; i++) {
+            neighborhoods[i] = parseInt(neighborhoods[i]);
+            // if it's the last item of the list
+            if (i === neighborhoods.length - 1) {
+                placeholders += '?';
+            }
+            // if it's NOT the last item of the list
+            else {
+                placeholders += '?, ';
+            }
+        }
+
+        if (queryParamCount === 1) {
+            sql += ' WHERE neighborhood_number IN (' + placeholders + ')';
+        }
+        else {
+            sql += ' AND neighborhood_number IN (' + placeholders + ')';
+        }
+
+        placeholders = '';
+        params = params.concat(neighborhoods);
+    }
+
+    // if 'limit' exists as a query parameter
+    if ('limit' in req.query) {
+        limit = req.query.limit;
+    }
+   
+    sql += ' ORDER BY date_time DESC LIMIT ' + limit;
+
+    //let sql = 'SELECT * FROM Incidents LIMIT 1000';
+    // let sql = 'SELECT case_number, date(date_time) AS date, time(date_time) AS time, code, incident, police_grid, neighborhood_number, block FROM Incidents ORDER BY date_time DESC LIMIT 1000';
+
+    dbSelect(sql, params)
     .then((rows) => {
 
         res.status(200).type('json').send(JSON.stringify(rows, null, 4));
